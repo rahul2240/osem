c389295d9e8641b865831a62cd2da56d07a90ce8
Rahul <rahulsingh2240@gmail.com>
2019-05-27 23:24:38 +0530

    Add invited users in controller

--- app/controllers/booths_controller.rb
+++ app/controllers/booths_controller.rb
@@ -94,6 +94,6 @@ class BoothsController < ApplicationController
 
   def booth_params
     params.require(:booth).permit(:title, :description, :reasoning, :state, :picture, :conference_id,
-                                  :created_at, :updated_at, :submitter_relationship, :website_url, responsible_ids: [])
+                                  :created_at, :updated_at, :submitter_relationship, :website_url, responsible_ids: [], invited_users: [])
   end
 end
