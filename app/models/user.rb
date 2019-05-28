0e09b8af06545082b094a8f256c8b7251333d3a0
Rahul <rahulsingh2240@gmail.com>
2019-05-28 23:51:37 +0530

    deliver now for emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -292,6 +292,6 @@ class User < ApplicationRecord
   end
 
   def send_devise_notification(notification, *args)
-    devise_mailer.send(notification, self, *args).deliver_later
+    devise_mailer.send(notification, self, *args).deliver_now
   end
 end
